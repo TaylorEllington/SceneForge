73b655fdc8b8546287f1195df78bd5cf92f3be07
Taylor Ellington <taylor.ellington@intel.com>
2018-10-05 17:25:14 -0700

    geting set up

---
+++ SceneForge/build-support/glfwConfig.cmake
@@ -0,0 +1,1 @@
+add_subdirectory(${LIB_DIR/glfw})
---
+++ SceneForge/src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "glfw/glfw3.h"
+#include "glm/common.hpp"
+#include "imgui.h"
+#include "vulkan/vulkan.h"
+using namespace std;
+
+int main() 
+{
+    cout << "Hello, World!";
+    return 0;
+}
